479a4eeea2f582aee03f206d1ca0272ec92a3a59
Hans Eggert <hans.eggert@gmx.de>
2015-05-12 12:57:48 +0200

    Add video data to data.js

--- js/data.js
+++ js/data.js
@@ -6,21 +6,230 @@ define([], function () {
 
     return {
     	// data goes here
-    	videos: {
-			"video1": videoBase + 'demo.mp4',
-			"video2": videoBase + 'video2.mp4', 
-			"etc": videoBase + 'pp.avi', 
+    	logo: {
+			url: imageBase + "bday.png"
+		},
+		videos: [
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},				{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+			{
+				url: videoBase + "demo.mp4",
+				thumb: imageBase + "demo.jpg",
+				alt: "Demo"
+			},
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            },
+            {
+                url: videoBase + "demo.mp4",
+                thumb: imageBase + "demo.jpg",
+                alt: "Demo"
+            }
 
-    	},
-    	images: {
-			"img1": imageBase + 'img2.jpg',
-			"img2": imageBase + 'img2.png',
-			"etc": imageBase + 'pp.jpg',
-			gifs: {
-				"g1": imageBase + 'g1.gif',
-				"h2": imageBase + 'h2.gif',
-				"etc": imageBase + 'pp.gif',
-			}
-    	}
+		]
     };
 });
--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-    require(["js/data.js"], function(data){
-        
-        console.log(data);
+require(["js/data"], function(data){
+    "use strict";
 
-    });
+    var POSITION_OF_LOGO = 6;
+    var VIDEO_WIDTH = 1280;
+    var VIDEO_HEIGHT = 640;
+
+    var LIGHTBOX_CLASS = "html5lightbox";
+
+    var $ = jQuery;
+
+    var $mainContainer = $("#links");
+
+    applyDataToDom($mainContainer, data);
+    require(["js/plugins/html5lightbox/html5lightbox"], function(){});
+
+    function applyDataToDom($target, data) {
+        for (var i = 0, len = data.videos.length; i < len; i++) {
+            if (i === POSITION_OF_LOGO) {
+                appendLogo($target, data.logo.url)
+            }
+            appendVideo($target, data.videos[i]);
+        }
+    }
+
+    function appendLogo($target, url) {
+        $target.append('<img class="logo" style="pointer-events: none;" src="' + url + '" alt="Happy Birthday!">');
+    }
+
+    function appendVideo($target, video) {
+        $target.append('<a href="' + video.url + '" class=' + LIGHTBOX_CLASS +
+            ' data-width=' + VIDEO_WIDTH +
+            ' data-height=' + VIDEO_HEIGHT +
+            '><img src=' + video.thumb + ' alt=' +
+            video.alt + '></a>');
+    }
+
+});
